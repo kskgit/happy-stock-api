194c8e1a4f8b838aef655195e2b5c0d3b4e93f9b
nakao keisuke <r3grashopper193@gmail.com>
2024-06-11 07:03:58 +0900

    feat:sparkles:pydemicの追加

--- src/happy_stock_api/__init__.py
+++ src/happy_stock_api/__init__.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-@app.get("/")
-def hello() -> str:
+class UserInput(BaseModel):
+    name: str
+    age: int
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+
+@app.get("/", response_model=User)
+async def hello(input: UserInput):
     return "Hello from happy-stock-api!"
